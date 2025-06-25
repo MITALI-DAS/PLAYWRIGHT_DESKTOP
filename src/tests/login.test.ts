17fc1ca6e4c734e055f8f877798b3b81d57fef42
mitali <mitali.d02@outlook.com>
2025-06-25 20:20:32 +0200

    one more chanhges added

--- src/tests/login.test.ts
+++ src/tests/login.test.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage} from '../pages/LoginPage'
 
+
 test('Valid to login  URL', async ({ page }) => {
 const loginPage = new LoginPage(page)
 await loginPage.goto()
